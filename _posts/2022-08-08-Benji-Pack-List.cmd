a7a1c69afc1cc68425272efaa63e8320ae6eb059
Paul Miller <pmiller@microsoft.com>
2022-08-09 05:11:53 +0000

    benji pack list

---
+++ _posts/2022-08-08-Benji-Pack-List.cmd
@@ -0,0 +1,62 @@
+--- 
+layout: post
+title: "Benji Pack list"
+author: "Paul Miller"
+tags: [hiking]
+comments: true
+---
+This is from a section k(?) hike. Stevens to Stehikin.
+
+### Pack 
+[Go lite 50L](http://www.golite.com/Jam-50L-Pack-Unisex-P46812.aspx)
+[Benji Pack]
+
+### Sleep
+- 2 Person Sumberjack tent? (setup )
+- stakes
+- REI Flash Sleeping bag 32. 
+- Benji Sleeping bag
+- 2  Sleeping mats
+### Cooking 
+-  Stove compatible with brad. 
+-  esbit cubes
+-  Pot 
+-  tiny lighter.
+-  spork
+### Clothes
+-  Three pair of compression shorts. 
+-  one pair shorts
+-  pants/long johns?
+-  one long sleeve (warm!) 
+-  rain jacket 
+-  garbage bag.
+### Feet
+-  2 pairs of socks each
+-  Normal shoes
+-  light sandals cut?
+### Hydration
+- Cheap plastic bottle and 1 nalgene or just bladder? 1quart
+- 40 qts purifier
+- gatorade/koolaid mixes
+### Cleaning
+- wet naps. 
+- Hand sanitizer
+- detergent. 
+- 1/2 sponge
+- Towel 
+- Toothbrush 
+- toilet paper?
+### Misc
+- S&W knife.(lighter knife). 
+- Flashlight (batteries) 
+- extra aaa batteis
+-  Kindle, kindle light. ?
+-  super small card deck. ?
+-  Rope. 
+-  100% deet
+### Food
+- Jerky
+- M&Ms
+- Mac and Cheese. 
+- Cuscous
+- oatmeal
